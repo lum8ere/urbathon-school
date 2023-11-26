f0b24ef2e30cd9984be6e12ea12e716207082ddd
Alexander <alexander.zaburdaev1904@gmail.com>
2023-11-26 12:38:23 +0300

    корсы

--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	_ "github.com/lib/pq"
 )
 
@@ -21,6 +22,7 @@ func init() {
 
 func main() {
 	app := fiber.New()
+	app.Use(cors.New())
 
 	app.Get("/api", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
